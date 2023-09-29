d9abd68244666a31091dd3d1d07a77a838629004
Axel Patricio Riffo Concha <82002334+AxelRiffo@users.noreply.github.com>
2023-09-29 10:49:10 -0300

    Correccion CSS

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.humanize.templatetags.humanize import intcomma
 from .models import Producto
 from .forms import RegistroForm, InicioSesionForm
-from django.contrib.auth.models import User
 
 def store(request):
     productos = Producto.objects.all()
@@ -44,3 +45,14 @@ def iniciar_sesion(request):
         form = InicioSesionForm()
 
     return render(request, 'inicio_sesion.html', {'form': form})
+
+def store(request):
+    productos = Producto.objects.all()
+
+    # Formatear el precio con punto como separador de miles y coma como separador decimal
+    for producto in productos:
+        precio_formateado = "${:,.0f}".format(producto.precio).replace(",", ".")
+        producto.precio_formateado = precio_formateado
+
+    context = {'productos': productos}
+    return render(request, 'store/store.html', context)
