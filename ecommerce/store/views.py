fa5000683ef2171af2699b05d05cce3e4838956a
Axel Patricio Riffo Concha <82002334+AxelRiffo@users.noreply.github.com>
2023-10-02 15:05:08 -0300

    Se creó el apartado para iniciar o registrar una cuenta

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -18,34 +18,6 @@ def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
 
-def registro(request):
-    if request.method == 'POST':
-        form = RegistroForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            User.objects.create_user(username=username, password=password)
-            return redirect('/')  # Página principal o donde desees redirigir después del registro
-    else:
-        form = RegistroForm()
-
-    return render(request, 'registro.html', {'form': form})
-
-def iniciar_sesion(request):
-    if request.method == 'POST':
-        form = InicioSesionForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('/')  # Página principal o donde desees redirigir después del inicio de sesión
-    else:
-        form = InicioSesionForm()
-
-    return render(request, 'inicio_sesion.html', {'form': form})
-
 def store(request):
     productos = Producto.objects.all()
 
@@ -56,3 +28,6 @@ def store(request):
 
     context = {'productos': productos}
     return render(request, 'store/store.html', context)
+
+def ingreso(request):
+    return render(request, 'store/ingreso.html')
