ee65eb0fd8949045392baa3f7c4b7dd03af69cde
SebastianEspinoza1 <80790905+SebastianEspinoza1@users.noreply.github.com>
2023-10-02 22:46:30 -0300

    copy (no c q mrd le pasa qq no veo los botones del nav))

--- ecommerce/ecommerce/static/js/carousel.js
+++ ecommerce/ecommerce/static/js/carousel.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelectorAll('.carousel-slide');
-    const prevButton = document.getElementById('prev');
-    const nextButton = document.getElementById('next');
     let currentIndex = 0;
 
     function showSlide(index) {
@@ -20,16 +18,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     showSlide(currentIndex);
-
-    prevButton.addEventListener('click', function () {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
-    });
-
-    nextButton.addEventListener('click', function () {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
-    });
-
     setInterval(autoSlide, 5000);
 });
