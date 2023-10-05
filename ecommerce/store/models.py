0fe8eeb5efa6885fcf4519a94b14e77cda156121
SebastianEspinoza1 <80790905+SebastianEspinoza1@users.noreply.github.com>
2023-10-05 14:46:00 -0300

    jiji

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
+from django.contrib.auth.models import User
 
 class Producto(models.Model):
     titulo = models.CharField(max_length=255)
@@ -11,3 +9,18 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Carrito de {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.titulo} en el carrito de {self.cart.user.username}"
--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('ingreso/', views.ingreso, name="ingreso"),
+    path('agregar_al_carrito/<int:producto_id>/', views.agregar_al_carrito, name="agregar_al_carrito"),
 ]
