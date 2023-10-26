4a4b9e0f91c27e90a0f46d4523f43bc3107d06f4
Axel Patricio Riffo Concha <82002334+AxelRiffo@users.noreply.github.com>
2023-10-26 14:48:04 -0300

    Arreglo css imagenes

--- APT2/store/static/js/loader.js
+++ APT2/store/static/js/loader.js
@@ -5,5 +5,5 @@ $(document).ready(function () {
     // Establece un temporizador para aplicar un fade out al elemento de carga después de 2 segundos (2000 milisegundos)
     setTimeout(function () {
         $('#loader-container').fadeOut(500); // 500 milisegundos para la animación de fade out
-    }, 100); // 2000 milisegundos para mostrar el elemento de carga
+    }, 500); // 2000 milisegundos para mostrar el elemento de carga
 });
