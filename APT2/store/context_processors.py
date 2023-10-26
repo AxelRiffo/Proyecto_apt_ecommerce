e72194aae151dd52bd83bc0e718a2cc30ef8ba81
SebastianEspinoza1 <80790905+SebastianEspinoza1@users.noreply.github.com>
2023-10-26 15:18:23 -0300

    cantidad en el carrito arreglado.

--- APT2/store/context_processors.py
+++ APT2/store/context_processors.py
@@ -1,7 +1,9 @@
 def total_carrito(request):
     total = 0
+    cantidad_productos = 0  # Variable para almacenar la cantidad de productos
     if request.user.is_authenticated:
         if "carrito" in request.session.keys():
             for key, value in request.session["carrito"].items():
-                total += int(value["cantidad"])
-    return {"total_carrito": total}
+                total += int(value["acumulado"])
+                cantidad_productos += int(value["cantidad"])  # Suma la cantidad de productos
+    return {"total_carrito": total, "cantidad_productos": cantidad_productos}
