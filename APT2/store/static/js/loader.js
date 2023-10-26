12fd4e2ca00b76c3fffda0b1d258a2c7161270ea
Axel Patricio Riffo Concha <82002334+AxelRiffo@users.noreply.github.com>
2023-10-26 13:56:16 -0300

    Arreglo css y cambio posicion carrito

--- APT2/store/static/js/loader.js
+++ APT2/store/static/js/loader.js
@@ -5,5 +5,5 @@ $(document).ready(function () {
     // Establece un temporizador para ocultar la carga después de 2 segundos (2000 milisegundos)
     setTimeout(function () {
         $('#loader-container').hide();
-    }, 1000);
+    }, 500);
 });
