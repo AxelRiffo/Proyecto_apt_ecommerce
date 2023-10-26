2dbc99edd9c63e888eb575a0c1a3184461317000
SebastianEspinoza1 <80790905+SebastianEspinoza1@users.noreply.github.com>
2023-10-26 15:05:09 -0300

    botón carrito muestra la cantidad acumulada

--- APT2/store/context_processors.py
+++ APT2/store/context_processors.py
@@ -3,5 +3,5 @@ def total_carrito(request):
     if request.user.is_authenticated:
         if "carrito" in request.session.keys():
             for key, value in request.session["carrito"].items():
-                total += int(value["acumulado"])
+                total += int(value["cantidad"])
     return {"total_carrito": total}
