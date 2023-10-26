754af6ab59d4f1b5a971a91b3463921795fa221d
Axel Patricio Riffo Concha <82002334+AxelRiffo@users.noreply.github.com>
2023-10-26 11:28:16 -0300

    Imagen de carga

---
+++ APT2/store/static/js/loader.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // Muestra el elemento de carga
+    $('#loader-container').show();
+
+    // Establece un temporizador para ocultar la carga después de 2 segundos (2000 milisegundos)
+    setTimeout(function () {
+        $('#loader-container').hide();
+    }, 1000);
+});
